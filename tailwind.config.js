df0e4baedcf4e5099e115f4dbfed194482ee0f23
Tom Sikora <tom.sikora03@gmail.com>
2024-08-07 19:11:45 +0200

    edit: tailwind.config.js
    Ajout de la configuration de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-gray': '#0a0a0b',
+        'middle-gray-1': '#0f0f10',
+        'middle-gray-2': '#121113',
+        'light-gray': '#232225',
+        'light-green': '#9DE62F',
+      },
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+        dmsans: ['DM Sans', 'sans-serif'],
+      },
+      screens: {
+        'ssm': '320px',
+        'msm': '375px',
+        'lsm': '425px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1600px',
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
