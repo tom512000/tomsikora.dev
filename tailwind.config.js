35bf937015f7cd0da6fba6a000ed8a905d48ddc7
Tom Sikora <tom.sikora03@gmail.com>
2024-08-29 12:14:04 +0200

    edit: tailwind.config.js
    Ajout des font size manquantes dans la configuration de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ module.exports = {
         '2xl': ['1.5rem', { lineHeight: '2rem' }],
         '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
         '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
-        '5xl': ['3rem', { lineHeight: '1rem' }],
+        '5xl': ['3rem', { lineHeight: '2.75rem' }],
       },
     },
   },
